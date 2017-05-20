aa41a548aa9bf116fa2f3ad64c96fad07c93b125
Vijay Vignesh <rush2vijay.g@gmail.com>
2017-05-20 23:01:34 +0530

    Initial commit

---
+++ conf.js
@@ -0,0 +1,46 @@
+
+
+exports.config = {
+
+
+	directConnect: true,
+
+	// Capabilities to be passed to the webdriver instance.
+	multiCapabilities: [{
+		browserName: 'chrome'
+	}],
+
+
+	framework: 'jasmine2',
+
+
+
+	specs: ['./functional/logintest.spec.js'],
+
+	/*suites: {
+
+			smoke: ['./smoke/!*.spec.js'],
+			regression: ['./regression/!*.spec.js'],
+			functional: ['./functional/!*.spec.js'],
+			all: ['./!*!/!*.spec.js'],
+			selected: ['./functional/addcustomer.spec.js','./regression/openaccount.spec.js'],
+
+
+	},*/
+
+
+
+	// Options to be passed to Jasmine.
+	jasmineNodeOpts: {
+		defaultTimeoutInterval: 30000
+	}
+
+
+
+
+
+
+
+
+
+}
